70f0863d99e4d82c0fb647bc19a864d895eab8aa
Isaac415 <isaac.cheng97@gmail.com>
2025-05-09 18:31:31 +0800

    Add 'use server'

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'alcenodlfaowuunmcvbt.supabase.co'
-            }
-        ]
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'alcenodlfaowuunmcvbt.supabase.co'
+        }
+      ]
     },
-    // Add this:
-    output: 'standalone' // or 'auto' if using Next.js 13+
-};
-
-export default nextConfig;
+    // Add these two options:
+    output: 'standalone',
+    experimental: {
+      appDir: true
+    }
+  };
+  
+  export default nextConfig;
